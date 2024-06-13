14d4e9b86f4500f43dd50f40375057488b643dda
Fiorella Irupaylla <fiorella.liz.irupaylla@gmail.com>
2024-06-12 22:21:01 -0500

    Actualizacion de cambios

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,214 @@
+<template>
+    <div class="wrapper" style="text-align: center">
+      <div class="title">
+        <q-icon name="ti-heart" color="white" size="35px" />
+        Clothes 4You
+      </div>
+      <form @submit.prevent="login">
+        <div class="field">
+          <input v-model="username" type="text" required />
+          <label>User</label>
+        </div>
+        <div class="field">
+          <input v-model="password" type="password" required />
+          <label>Password</label>
+        </div>
+        <div class="content">
+          <div class="checkbox">
+            <input type="checkbox" id="remember-me" />
+            <label for="remember-me">Remember me</label>
+          </div>
+          <div class="pass-link">
+            <a href="#">Forgot password?</a>
+          </div>
+        </div>
+        <div class="field">
+          <q-btn label="Login" type="submit" color="red" class="custom-btn" />
+        </div>
+        <div class="signup-link">Not a member? <a href="#">Signup now</a></div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: "",
+        password: "",
+      };
+    },
+    methods: {
+      login() {
+        // Validar los campos de usuario y contraseña antes de enviar
+        if (this.username.trim() === "" || this.password.trim() === "") {
+          alert("Por favor completa los campos de usuario y contraseña.");
+          return;
+        }
+  
+        // Aquí puedes agregar la lógica para enviar el formulario
+        console.log("Username:", this.username);
+        console.log("Password:", this.password);
+  
+        // Simular redirección después de iniciar sesión
+        window.location.href = "http://localhost:9000/#/sales";
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  @import url("https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap");
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
+  html,
+  body {
+    display: grid;
+    height: 100%;
+    width: 100%;
+    place-items: center;
+    background: #f2f2f2;
+    /* background: linear-gradient(-135deg, #c850c0, #4158d0); */
+  }
+  ::selection {
+    background: red;
+    color: #fff;
+  }
+  .wrapper {
+    width: 380px;
+    background: #fff;
+    border-radius: 15px;
+    box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.1);
+    text-align: center;
+    align-items: center;
+  }
+  .wrapper .title {
+    font-size: 35px;
+    font-weight: 600;
+    text-align: center;
+    line-height: 100px;
+    color: #fff;
+    user-select: none;
+    border-radius: 15px 15px 0 0;
+    background: linear-gradient(-135deg, #c85050, red);
+  }
+  .wrapper form {
+    padding: 10px 30px 50px 30px;
+  }
+  .wrapper form .field {
+    height: 50px;
+    width: 100%;
+    margin-top: 20px;
+    position: relative;
+  }
+  .wrapper form .field input {
+    height: 100%;
+    width: 100%;
+    outline: none;
+    font-size: 17px;
+    padding-left: 20px;
+    border: 1px solid lightgrey;
+    border-radius: 25px;
+    transition: all 0.3s ease;
+  }
+  .wrapper form .field input:focus,
+  form .field input:valid {
+    border-color: #d09741;
+  }
+  .wrapper form .field label {
+    position: absolute;
+    top: 50%;
+    left: 20px;
+    color: #999999;
+    font-weight: 400;
+    font-size: 17px;
+    pointer-events: none;
+    transform: translateY(-50%);
+    transition: all 0.3s ease;
+  }
+  form .field input:focus ~ label,
+  form .field input:valid ~ label {
+    top: 0%;
+    font-size: 16px;
+    color: #4158d0;
+    background: #fff;
+    transform: translateY(-50%);
+  }
+  form .content {
+    display: flex;
+    width: 100%;
+    height: 50px;
+    font-size: 16px;
+    align-items: center;
+    justify-content: space-around;
+  }
+  form .content .checkbox {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  form .content input {
+    width: 15px;
+    height: 15px;
+    background: red;
+  }
+  form .content label {
+    color: #262626;
+    user-select: none;
+    padding-left: 5px;
+  }
+  form .content .pass-link {
+    color: "";
+  }
+  form .field input[type="submit"] {
+    color: #fff;
+    border: none;
+    padding-left: 0;
+    margin-top: -10px;
+    font-size: 20px;
+    font-weight: 500;
+    cursor: pointer;
+    background: linear-gradient(-135deg, #c85050, red);
+    transition: all 0.3s ease;
+  }
+  form .field input[type="submit"]:active {
+    transform: scale(0.95);
+  }
+  form .signup-link {
+    color: #262626;
+    margin-top: 20px;
+    text-align: center;
+  }
+  form .pass-link a,
+  form .signup-link a {
+    color: #4158d0;
+    text-decoration: none;
+  }
+  form .pass-link a:hover,
+  form .signup-link a:hover {
+    text-decoration: underline;
+  }
+  
+  .custom-btn {
+    color: #fff;
+    border: none;
+    padding-left: 0;
+    margin-top: -10px;
+    font-size: 20px;
+    font-weight: 500;
+    cursor: pointer;
+    background-color: rgb(227, 17, 17);
+    transition: all 0.3s ease;
+    border-radius: 25px;
+    width: 90%;
+    height: auto;
+  }
+  
+  .custom-btn:active {
+    transform: scale(0.95);
+  }
+  </style>
---
+++ src/pages/product/SalesProductForm.vue
@@ -0,0 +1,218 @@
+<template>
+    <q-layout view="hHh lpR fFf" class="bg-grey-1">
+      <q-header elevated class="bg-white text-grey-8 q-py-xs" height-hint="58">
+        <q-toolbar>
+          <q-btn
+            flat
+            dense
+            round
+            @click="toggleLeftDrawer"
+            aria-label="Menu"
+            icon="menu"
+          />
+  
+          <q-btn flat no-caps no-wrap class="q-ml-xs" v-if="$q.screen.gt.xs">
+            <q-icon name="ti-heart" color="red" size="28px" />
+            <q-toolbar-title shrink class="text-weight-bold">
+              Clothes 4You
+            </q-toolbar-title>
+          </q-btn>
+  
+          <q-space />
+  
+          <div class="YL__toolbar-input-container row no-wrap">
+            <q-input dense outlined square v-model="search" placeholder="Buscar Producto" class="bg-white col" />
+            <q-btn  color="grey-3" text-color="red" icon="search" unelevated />
+          </div>
+  
+          <q-space />
+  
+          <div class="q-gutter-sm row items-center no-wrap">
+            
+            <q-btn round dense flat color="grey-8" icon="apps" v-if="$q.screen.gt.sm">
+              <q-tooltip>Catalogos</q-tooltip>
+            </q-btn>
+            <q-btn round dense flat color="grey-8" icon="message" v-if="$q.screen.gt.sm">
+              <q-tooltip>Mensajes</q-tooltip>
+            </q-btn>
+            <q-btn round dense flat color="grey-8" icon="shopping_cart">
+              <q-badge color="red" text-color="white" floating>
+                2
+              </q-badge>
+              <q-tooltip>Carrito de Compras</q-tooltip>
+            </q-btn>
+            <q-btn round flat>
+              <q-avatar size="26px">
+                <img src="https://cdn.quasar.dev/img/avatar.png">
+              </q-avatar>
+              <q-tooltip>Mi cuenta</q-tooltip>
+            </q-btn>
+          </div>
+        </q-toolbar>
+      </q-header>
+  
+      <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        bordered
+        class="bg-grey-2"
+        :width="240"
+      >
+        <q-scroll-area class="fit">
+          <q-list padding>
+            <q-item v-for="link in links1" :key="link.text" v-ripple clickable>
+              <q-item-section avatar>
+                <q-icon color="grey" :name="link.icon" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ link.text }}</q-item-label>
+              </q-item-section>
+            </q-item>
+  
+            <q-separator class="q-my-md" />
+  
+            <q-item v-for="link in links2" :key="link.text" v-ripple clickable>
+              <q-item-section avatar>
+                <q-icon color="grey" :name="link.icon" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ link.text }}</q-item-label>
+              </q-item-section>
+            </q-item>
+  
+            <q-separator class="q-mt-md q-mb-xs" />
+          
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+  
+      <q-page-container class="q-ma-md" style="height: 80vh;">
+    <div class="q-pa-md" style="max-width: 80%; margin: 0 auto;">
+      <q-carousel
+        v-model="slide"
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        swipeable
+        animated
+        control-color="red"
+        navigation
+        padding
+        arrows
+        height="100%"
+        class="bg-white-9 shadow-3 rounded-borders"
+      >
+        <q-carousel-slide :name="1" class="column no-wrap">
+          <div class="row justify-center items-center q-gutter-xs q-col-gutter no-wrap">
+            <q-card class="col-4 full-height">
+              <q-img class="rounded-borders col-4 full-height" src="../../img_products/1-p.jpg" />
+              <q-card-section class="text-center">
+                <q-card-text>
+                    <q-text h4>MANGO</q-text h4>
+                    <q-text h6><br>$440.00</q-text h6> 
+                </q-card-text>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="col-4 full-height">
+              <q-img class="rounded-borders col-4 full-height" src="../../img_products/3-p.jpg" />
+              <q-card-section class="text-center">
+                <q-card-text>
+                    <q-text h4>MANGO</q-text h4>
+                    <q-text h6>BOTTOMS<br>$440.00</q-text h6> 
+                </q-card-text>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="col-4 full-height">
+              <q-img class="rounded-borders col-4 full-height" src="../../img_products/2-p.jpg" />
+              <q-card-section class="text-center">
+                <q-card-text>
+                    <q-text h4>MANGO</q-text h4>
+                    <q-text h6>$440.00</q-text h6> 
+                </q-card-text>
+              </q-card-section>
+            </q-card>
+          </div>
+        </q-carousel-slide>
+
+        <q-carousel-slide :name="2" class="column no-wrap">
+          <div class="row justify-center items-center q-gutter-xs q-col-gutter no-wrap">
+            <q-card class="col-4 full-height">
+              <q-img class="rounded-borders col-4 full-height" src="../../img_products/5.jpg" />
+              <q-card-section class="text-center">
+                <q-card-text>
+                  <q-text h4>MANGO</q-text h4>
+                  <q-text h6>BOTTOMS<br>$440.00</q-text h6> 
+                </q-card-text>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="col-4 full-height">
+              <q-img class="rounded-borders col-4 full-height" src="../../img_products/6.jpg" />
+              <q-card-section class="text-center">
+                <q-card-text>
+                  <q-text h4>MANGO</q-text h4>
+                  <q-text h6>BOTTOMS<br>$440.00</q-text h6> 
+                </q-card-text>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="col-4 full-height">
+              <q-img class="rounded-borders col-4 full-height" src="../../img_products/5.jpg" />
+              <q-card-section class="text-center">
+                <q-card-text>
+                    <q-text h4>MANGO</q-text h4>
+                    <q-text h6>BOTTOMS<br>$440.00</q-text h6> 
+                </q-card-text>
+              </q-card-section>
+            </q-card>
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    </div>
+  </q-page-container>
+    </q-layout>
+  </template>
+  <script>
+  import { ref } from 'vue'
+  import { fabYoutube } from '@quasar/extras/fontawesome-v6'
+  
+  export default {
+    name: 'MyLayout',
+  
+    setup () {
+
+     
+      const slide = ref(1) 
+      const leftDrawerOpen = ref(false)
+      const search = ref('')
+        
+      function toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+  
+      return {
+        fabYoutube,
+        slide,
+        leftDrawerOpen,
+        search,
+  
+        toggleLeftDrawer,
+  
+        links1: [
+          { icon: 'home', text: 'Inicio' },
+          { icon: 'whatshot', text: 'Tendencias 2024' },
+          { icon: 'subscriptions', text: 'Mis Suscripciones' }
+        ],
+        links2: [
+          { icon: 'apps', text: 'Catálogos' },
+          { icon: 'restore', text: 'Historial de Compras' },
+          { icon: 'ti-user', text: 'Mi Cuenta' },
+          { icon: 'thumb_up_alt', text: 'Mis Favoritos' },
+          { icon: 'ti-control-record', text: 'Salir' }
+          
+        ]
+      }
+    }
+  }
+  </script>
